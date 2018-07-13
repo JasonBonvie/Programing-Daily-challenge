9595ebaf16001f0f3f99f7ca2dfa93355fe1f871
jasonb3194@yahoo.com <jasonb3194@gmail.com>
2018-07-13 01:11:01 -0400

    final push for the calculator project

--- Cclass/C_Memory_Calc/calculator.c
+++ Cclass/C_Memory_Calc/calculator.c
@@ -10,14 +10,14 @@ float mult(float A, float B);
 double powe(float A, float B);
 float fact(float A, float B);
 
-
 int main(){
      welcome();
      return 0 ; 
 }
 
 void welcome(){
-	while(1 == 1 ){
+	char yon = 'y';
+	while(yon == 'y'){
 		float A;
 		float B;
 		char Op = 'k';
@@ -28,6 +28,7 @@ void welcome(){
 		printf("[1] Insert two operands (A and B) and an operator\n");
 		printf("[2] Use the Previous result as operand A, insert operand B and an operator\n");
 		int selection;
+		// scanning for the option to use custom enteries or the entery from previous calculation
 		scanf("%d", &selection);
 		if (selection == 1){
 
@@ -38,28 +39,36 @@ void welcome(){
 			printf("Please enter the operation you would like to preform. \n");
 			scanf("%s", &Op);
 			printf("%c", Op );
-
-
+            // conditonal statements for all of the operators supported. 
 			if (Op == '+'){
 				result = sum(A,B);
 				printf("The sum is : %f\n", result);
+				continue;
 			}
 			if (Op == '-'){
 				result = sub(A,B);
 				printf("The differance is : %f\n", result);
+				continue;
 			}
 			if (Op == '*'){
 				result = mult(A,B);
 				printf("The product is : %f\n", result);
+				continue;
 			}
 			if (Op == 'P'){
 				result = powe(A,B);
 				printf("The A raised to the power B is : %f\n", result);
+				continue;
 			}
 			if (Op == '!'){
 				result = fact(A,B);
 				printf("The factorial of A + B (where the factorial of N = 1 x 2 x 3 x 4 x ... x N) %f\n", fact(A,B));
+				continue;
 			}
+			else{
+				printf("The operator you entered is not supported.\n");
+			}
+			
 		}
 
 		if (selection == 2){
@@ -68,60 +77,84 @@ void welcome(){
 			scanf("%f", &B);
 			printf("Please enter the operation you would like to preform. \n");
 			scanf("%s", &Op);
-			printf("%c", Op );
+			// conditonal statements for all of the operators supported. 
+			
 	        if (Op == '+'){
 				result = sum(result,B);
 				printf("The sum is : %f\n", result);
+				continue;
 			}
 			if (Op == '-'){
 				result = sub(result,B);
 				printf("The differance is : %f\n", result);
+				continue;
 			}
 			if (Op == '*'){
 				result = mult(result,B);
 				printf("The product is : %f\n", result);
+				continue;
 			}
 			if (Op == 'P'){
 				result = powe(result,B);
 				printf("The A raised to the power B is : %f\n", result);
+				continue;
 			}
 			if (Op == '!'){
 				result = fact(result,B);
 				printf("The factorial of A + B (where the factorial of N = 1 x 2 x 3 x 4 x ... x N) %f\n", result);
+			
+				continue;
 			}
+			else{
+				printf("The operator you entered is not supported.");
+			}
+			
 		}
+		printf("would you like to preform another calculation? y/n\n");
+		scanf("%s", &yon); 
 	}
 
 }
-
+// Preforms the sum of two numbers
 float sum(float A , float B){
 	float sum; 
 	sum = A + B; 
 	return sum; 
 }
-
+// Returns the differance of two numbers
 float sub(float A, float B){
 	float sub; 
 	sub = A - B; 
 	return sub; 
 }
+// Returns the procuct of two numbers
 float mult(float A, float B){
 	float mult;
 	mult = A * B;
 	return mult; 
 }
+// Returns A raised to the B and returns message if one of the numbers is negative
 double powe(float A, float B){
 	double a; 
 	double b; 
 	double p; 
+	if ((A < 0) || (B < 0)){
+		printf("sorry you entered a negative number");
+		return -1; 
+	}
 	a = (double)A; 
 	b = (double)B; 
 	p = pow(a, b);  
 	return p;
 }
+// Returns A plus B factorial of posative numers only 
 float fact(float A, float B){
 	float N = A + B; 
 	float factorial = 1; 
+	if ((A < 0) || (B < 0)){
+		printf("sorry you entered a negative number");
+		return -1; 
+	}
 	for (int i = 1; i <= N; i++){
 		factorial *= i;
 	}
