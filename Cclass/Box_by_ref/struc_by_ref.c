99d6fb31e1422fd911b0c96f7e11f7388e6080e9
jasonb3194@yahoo.com <jasonb3194@gmail.com>
2018-07-20 02:56:50 -0400

    final commit on 2 practice projects

--- Cclass/Box_by_ref/struc_by_ref.c
+++ Cclass/Box_by_ref/struc_by_ref.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <math.h>
 
-
+// How the "box" structure is formed 
 struct Box{
 	char *name; 
 	int length;
@@ -17,7 +17,7 @@ void computeBox(struct Box *B);
 int compareBox(struct Box a, struct Box b);
 
 int main(){
-	struct Box R1 = {"Rec1", 10, 3, 4, 0, 0 };
+	struct Box R1 = {"Rec1", 17, 3, 4, 0, 0 };
 	struct Box R2 = {"Rec2", 15, 2, 5, 0, 0 };
 	computeBox(&R1);
 	computeBox(&R2);
@@ -26,14 +26,15 @@ int main(){
 	printf("%d\n",compareBox(R1,R2)); 
 
 }
-
+// takes in a referance to the box structure 
 void computeBox(struct Box *B){
+	// pointers are used to show the locations of the box information 
 	 B->area = B->length*B->width*B->height; 
 	 B->parameter = B->length*2+B->width*2; 
 };
 
 void printBox(struct Box b){
-
+	// this prints the box information 
 	printf("~~~~ YOUR BOX ~~~~ \nThe Name is: %s\n",b.name);
 	printf("The length is: %d\n",b.length);
 	printf("The width is: %d\n",b.width);
@@ -42,7 +43,7 @@ void printBox(struct Box b){
 	printf("The parameter is: %d\n",b.parameter);
 	
 };
-
+// compares the boxes and returns information as requested 
 int compareBox(struct Box r1, struct Box r2){
 	if(r1.area > r2.area){
 		return 1;
